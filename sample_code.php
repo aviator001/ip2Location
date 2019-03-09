769f6b91774ca5952cf3a749edbc59958098a534
aviator001 <g@txt.am>
2019-03-08 23:09:37 -0800

    Update sample_code.php

--- sample_code.php
+++ sample_code.php
@@ -1,30 +1,60 @@
 <?
-	/*
-		USAGE 1: IP Address optional
-		When no IP is passed, it automatically detects 
-		the remote users IP and uses that
-		
-		$result1=$ip->locate();
-		
-		USAGE 2: Pass IP Address
-		It will provide the location for the IP address passed
-		$result2=$ip->locate('8.8.8.8');
-	
-	*/
-	include "class/ip.locate.class.php";
-	$ip=new ipLocate;
-	$result1=$ip->locate();
-	
-	//Thats all the code thats needed to get results.
-	//Shown below are other options
-	
-	$result2=$ip->locate('45.56.150.112');
-	
-	//Convert json string to array
-	print_r(json_decode($result1));
-	print_r(json_decode($result1));
-	
-	//To parse individual data, for example city, do like so:
-	$city=json_decode($result1)[0]->city;
-	print_r($city);
-?> 
+/* Code Sample 1 in PHP */
+/* OBJECTIVE */
+/* In this example, we are going to detect a users IP address */
+
+/* Instantiate Library */
+include "class/ip.locate.class.php";
+
+/* Create New Object fron library */
+$ip=new ipLocate;
+
+/* call the locate method, which auto detects the users IP address and then converts it into a location array */
+$resuiplt=$ip->getIP();
+
+/* Debug Output */
+$p->show($data);
+
+/* Code Sample 2 in PHP */
+/* OBJECTIVE */
+/* In this example, we are going to convert a users given IP address into location */
+
+/* Instantiate Library */
+include "class/ip.locate.class.php";
+
+/* Create New Object fron library */
+$ip=new ipLocate;
+
+/* call the locate method, and pass in the IP address we want to covert into a location */
+$result=$ip->locate('104.32.52.60');
+
+/* Decodee JSON into an array */
+$data=json_decode($result);
+
+/* Prase individual data poinst and echo output */
+echo $location=$data->city . " " . $data->state;
+
+/* Debug Output */
+$p->show($data);
+
+/* Code Sample 3 in PHP */
+/* OBJECTIVE */
+/* In this example, we are going to get the location for a user visiting for the first time, ie no IP address given */
+
+/* Instantiate Library */
+include "class/ip.locate.class.php";
+
+/* Create New Object fron library */
+$ip=new ipLocate;
+
+/* call locate method with no input, making it auto detect the IP address and then converting into a array of location info* /
+$result=$ip->locate();
+
+/* Decodee JSON into an array */
+$data=json_decode($result);
+
+/* Prase individual data poinst and echo output */
+echo $location=$data->city . " " . $data->state;
+
+/* Debug Output */
+$p->show($data);
